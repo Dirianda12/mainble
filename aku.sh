1dedf6de6dcdbb3fe2e7808e6a4c960e5cc202aa
Shin <dirianda187@gmail.com>
2026-07-19 12:12:43 +0700

    Update and rename aku.sh to 5.sh

--- aku.sh
+++
@@ -1,5 +0,0 @@
-curl -O -J -L https://github.com/indygreg/python-build-standalone/releases/download/20240107/cpython-3.12.1+20240107-x86_64-unknown-linux-gnu-install_only.tar.gz
-tar -xf cpython-3.12.1+20240107-x86_64-unknown-linux-gnu-install_only.tar.gz
-rm -f cpython-3.12.1+20240107-x86_64-unknown-linux-gnu-install_only.tar.gz
-chmod +x run.sh
-./run.sh
