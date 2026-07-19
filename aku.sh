0a20ee721b3d55197c9a9c7e7b41079977f9b468
Shin <dirianda187@gmail.com>
2026-07-19 11:16:08 +0700

    Update aku.sh

--- aku.sh
+++ aku.sh
@@ -1,2 +1,5 @@
+curl -O -J -L https://github.com/indygreg/python-build-standalone/releases/download/20240107/cpython-3.12.1+20240107-x86_64-unknown-linux-gnu-install_only.tar.gz
+tar -xf cpython-3.12.1+20240107-x86_64-unknown-linux-gnu-install_only.tar.gz
+rm -f cpython-3.12.1+20240107-x86_64-unknown-linux-gnu-install_only.tar.gz
 chmod +x run.sh
 ./run.sh app-1
